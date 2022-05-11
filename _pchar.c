edc3285a1bb3c5035447028dcdc856681f228078
comels <come.l.s@hotmail.fr>
2022-05-11 13:01:15 +0000

    pchar

--- _pchar.c
+++ _pchar.c
@@ -9,13 +9,13 @@ void _pchar(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pchar, stack empty", line_number);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		error = 1;
 		return;
 	}
 	if ((*stack)->n > 127 || (*stack)->n < 0)
 	{
-		fprintf(stderr, "L%d: can't pchar, value out of range", line_number);
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		error = 1;
 		return;
 	}
