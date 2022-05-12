bfed7b9318847baa541b4e40135a7e8d68d47ffd
comels <come.l.s@hotmail.fr>
2022-05-12 10:07:25 +0000

    commentaires des fonctions

--- monty.h
+++ monty.h
@@ -61,5 +61,6 @@ void _mod(stack_t **stack, unsigned int line_number);
 void _pchar(stack_t **stack, unsigned int line_number);
 void _pstr(stack_t **stack, unsigned int line_number);
 void _rotl(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number);
 
 #endif
